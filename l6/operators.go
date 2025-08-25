0cc186b34a3c5994f7d16fcf0c5ed6960dd659c5
Piyush0053 <piyush.m@razorpay.com>
2025-08-25 14:02:20 +0530

    operators

---
+++ l6/operators.go
@@ -0,0 +1,35 @@
+package main
+import ("fmt")
+
+func main() {
+  var a = 15 + 25
+  fmt.Println(a)
+
+
+//   arithmetic operators
+
+var (
+    sum1 = 100 + 50
+    sum2 = sum1 + 250 
+    sum3 = sum2 + sum2 
+  )
+  fmt.Println(sum3)
+
+
+/*Operator	Name	Description	Example	Try it
++	Addition	Adds together two values	x + y	
+-	Subtraction	Subtracts one value from another	x - y	
+*	Multiplication	Multiplies two values	x * y	
+/	Division	Divides one value by another	x / y	
+%	Modulus	Returns the division remainder	x % y	
+++	Increment	Increases the value of a variable by 1	x++	
+--	Decrement	Decreases the value of a variable by 1	x--
+
+*/
+
+
+// Assignment Operators
+
+
+
+}
