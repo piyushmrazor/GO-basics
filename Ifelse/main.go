1fab922558305a7c97b9c4f28f63e3a868521da9
Piyush0053 <piyush.m@razorpay.com>
2025-08-25 14:17:29 +0530

    ifelse

---
+++ Ifelse/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main(){
+	// age :=18
+
+	// if age>=18{
+	// 	fmt.Println("person is an adult")
+
+	// }  else{
+	// 	fmt.Println("person is not an adult")
+	// }
+
+	// age:=16
+
+	// if age>=18{
+	// 	fmt.Println("person is an adult")
+	// }		else if age>=13{
+	// fmt.Println("person is a teenager")
+	// 	}	else{
+	// 		fmt.Println("person is a child")
+	// 	}
+
+
+	// var role = "admin"
+
+	// var hasPermisiion = false
+
+	// if role=="admin" || hasPermisiion {
+
+	// 	fmt.Println("yes")
+	// }
+	// we can declare a variable inside if construct
+	
+	if age:= 20; age>=18{
+		fmt.Println("person" , age)
+		}else if age>=12 {
+		fmt.Println("child", age)
+	}
+
+}
