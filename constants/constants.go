a0c88c187aab08e0521b1c08f1bdfbd8894f4b77
Piyush0053 <piyush.m@razorpay.com>
2025-08-25 14:03:07 +0530

    constant

---
+++ constants/constants.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	const name= "golang"
+
+	// we could not change or add the  in the 
+	// constant on the run time
+
+	fmt.Println("hello world 2 ",name);
+
+
+
+
+}
