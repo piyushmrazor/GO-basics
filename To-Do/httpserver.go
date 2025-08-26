f1ed191355eafa8e64d86c4c332a3de543a8e0ae
Piyush0053 <piyush.m@razorpay.com>
2025-08-26 15:48:23 +0530

    todo

---
+++ To-Do/httpserver.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Start() {
+	fmt.Println("HELLO WORLD 2")
+}
--- To-Do/main.go
+++ To-Do/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 // Declaring the variable globally
@@ -22,27 +23,31 @@ func addTasks(array *[]string, newTask string) {
 
 func main() {
 	//var name = "######## Welcome #########"
-	//fmt.Println("Watch go crash course")
+	fmt.Println("Watch go crash course")
 	//fmt.Println("Watch here the youtuber channel")
 	//fmt.Println("Learning the Golang")
 	//
 	//fmt.Println("")
 
 	//Slice arrays
-	var array = []string{"Watch here the youtuber channel", "Watch go crash course", "Learning the Golang"}
-	//fmt.Println(array)
-	fmt.Println("Function is calling ")
-	//Calling the function
-
-	//we can pass as many parametes as possible in the functions
+	//var array = []string{"Watch here the youtuber channel", "Watch go crash course", "Learning the Golang"}
+	////fmt.Println(array)
+	//fmt.Println("Function is calling ")
+	////Calling the function
+	//
+	////we can pass as many parametes as possible in the functions
+	////PrintTasks(array)
+	//var newtasks = "hey there"
+	//addTasks(&array, newtasks)
+	//
+	//fmt.Println("Function is executed ")
 	//PrintTasks(array)
-	var newtasks = "hey there"
-	addTasks(&array, newtasks)
-
-	fmt.Println("Function is executed ")
-	PrintTasks(array)
 	//for idx, i := range array {
 	//	fmt.Println(idx+1, ".", i)
 	//}
+
+	//Making the http server
+	//*************************************
+	http.ListenAndServe(":8080", nil)
 
 }
