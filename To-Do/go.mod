1eb941432b45dcc5f012bcc84e75b8899eea08f3
Piyush0053 <piyush.m@razorpay.com>
2025-08-26 12:49:48 +0530

    Basic To-do functions

---
+++ To-Do/go.mod
@@ -0,0 +1,1 @@
+module To-Do
---
+++ To-Do/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Declaring the variable globally
+//var array = []string{"Watch here the youtuber channel", "Watch go crash course", "Learning the Golang"}
+
+// Defining the parameters in the functions with their datatypes
+func PrintTasks(array []string) {
+	for idx, i := range array {
+		fmt.Println(idx+1, ".", i)
+	}
+}
+
+// Here the list for the addded new task
+func addTasks(array *[]string, newTask string) {
+	*array = append(*array, newTask)
+	PrintTasks(*array)
+}
+
+func main() {
+	//var name = "######## Welcome #########"
+	//fmt.Println("Watch go crash course")
+	//fmt.Println("Watch here the youtuber channel")
+	//fmt.Println("Learning the Golang")
+	//
+	//fmt.Println("")
+
+	//Slice arrays
+	var array = []string{"Watch here the youtuber channel", "Watch go crash course", "Learning the Golang"}
+	//fmt.Println(array)
+	fmt.Println("Function is calling ")
+	//Calling the function
+
+	//we can pass as many parametes as possible in the functions
+	//PrintTasks(array)
+	var newtasks = "hey there"
+	addTasks(&array, newtasks)
+
+	fmt.Println("Function is executed ")
+	PrintTasks(array)
+	//for idx, i := range array {
+	//	fmt.Println(idx+1, ".", i)
+	//}
+
+}
